cfef1193267b8e32f1d6f5523ec5ec1e8ce53fe5
Matěj <mckillem@tuta.io>
2023-10-23 08:52:58 +0200

    přidání o mě

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,5 +1,14 @@
+import "./About.css";
 export const About = () => {
 	return (
-		<>About</>
+		<>
+			<h2>Záliby</h2>
+			<p>Technologie, programování, pochody, hodnotný hovor</p>
+			<h2>Moje tvorba</h2>
+			<ul className="projects">
+				<li><a href="https://github.com/mckillem/prepinaci-stranka">Jednoduché přepínání v Reactu</a></li>
+				<li><a href="https://github.com/mckillem/Blog-React">Blog v Reactu</a></li>
+			</ul>
+		</>
 	);
 };
