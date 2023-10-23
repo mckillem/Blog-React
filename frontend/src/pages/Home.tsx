b55ed08cd6c4e010eb586f5bceebdfca08f685f2
Matěj <mckillem@tuta.io>
2023-10-23 09:15:00 +0200

    přidání domů a novinek

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,17 @@
 export const Home = () => {
 	return (
-		<>Home</>
+		<>
+			<h1>Kudy domů</h1>
+			<img src="https://www.cestadomu.cz/sites/default/files/aktuality/dsc02385.jpg" width="400" alt=""/>
+			<p>Když kupujete pozemek, logicky předpokládáte, že se na něj také budete moci dostat. Jenže někdy je všechno jinak. Zvláště na vesnici se poměrně často stává, že jste léta jezdili přes pozemek souseda, kterému to nevadilo. Ten ale zemřel a jeho dědicům se vaše „vychozené právo“ nelíbí. Nebo jste třeba získali pozemek zpět v restituci, jenže ouha, příjezdová cesta k němu chybí. Naštěstí i nový občanský zákoník na tzv. právo nezbytné cesty pamatuje a věnuje se tomuto institutu dokonce podrobněji než předchozí norma.</p>
+			<h2>Veřejná cesta</h2>
+			<img src="https://g.denik.cz/21/ea/pod_nemocnici_kt_denik-630-16x9.jpg" alt=""/>
+			<p>Obecně lze říci, že přístup k nemovité věci není zajištěn, pokud není napojena na veřejnou cestu. Veřejnou cestou nemusí být jen běžná místní komunikace, která je většinou na pozemku obce. Může se jednat také o tzv. veřejně přístupnou účelovou komunikaci, která nemusí mít ani zpevněný povrch (tzv. polňačka) a může vést dokonce i přes pozemek soukromého vlastníka. Elementárním znakem veřejné cesty je její veřejné užívání, tedy to, že ji využívá širší veřejnost. Veřejnou cestou tedy nebude vjezd k jednomu domu, který je užíván pouze pro přístup k tomuto domu.</p>
+			<p>Máte-li pochybnosti, zda se jedná o veřejnou cestu, či nikoliv, lze se obrátit na silniční správní úřad, který o tom vydá deklaratorní rozhodnutí. V menších obcích tuto funkci zastává starosta.</p>
+			<h2>Zřízení služebnosti</h2>
+			<img src="https://www.v6-zavody.cz/fotogalerie/kudy_1.jpg" alt=""/>
+			<p>Pokud vaše nemovitost není s žádnou veřejnou cestou dostatečně spojena, je potřeba pokusit se řešit tuto situaci se sousedy. V ideálním případě se dohodnete se sousedem na odkoupení části pozemku, případně zřízení úplatné služebnosti (dříve věcného břemene) spočívající v právu chůze a jízdy přes sousední pozemek s přesně vymezeným rozsahem zakresleným v geometrickém plánu.</p>
+			<p>Tato smlouva by měla být sjednána na dobu neurčitou. Tzv. panujícím pozemkem bude ten váš, ke kterému zřizujete přístup. Smlouvu o zřízení služebnosti poté musíte nechat zavkladovat do katastru nemovitostí.</p>
+		</>
 	);
 };
--- frontend/src/pages/News.tsx
+++ frontend/src/pages/News.tsx
@@ -1,5 +1,19 @@
 export const News = () => {
 	return (
-		<>aktuality</>
+		<>
+			<h1>iOS 17 Features: The Ultimate Mega Guide</h1>
+			<img src="https://images.macrumors.com/t/Iadz44xJSACogk57M1j0nLPrkj8=/800x0/article-new/2023/09/iOS-17-Ultimate-Guide-Feature-2.5.jpg?lossy" alt=""/>
+			<p>This comprehensive guide rounds up all of our coverage, making it a great resource for casual users who haven't had a chance to test out ‌iOS 17‌ and those who might not remember all of the new features. Everything is organized by app or feature to make it easy to find what you might be looking for.</p>
+			<h2>Lock Screen Updates</h2>
+			<p>Apple made further updates to the Lock Screen in ‌iOS 17‌ with StandBy, a feature that turns an iPhone into a home hub when it's charging and positioned in a horizontal orientation. The update also brings interactive widgets and other features.</p>
+			<img src="https://images.macrumors.com/t/tedkmRAA5pGSiHA1oNdHJk0IODU=/800x0/article-new/2023/06/iOS-17-Lockscreen-Feature.jpg?lossy" alt=""/>
+			<h2>Messages</h2>
+			<p>The Messages app got a redesign that makes it less cluttered and simpler to use, plus it has a revamp to the stickers function. Emoji are now stickers and can be placed anywhere on an iMessage, and you can even create your own stickers from photos. A new Check In feature keeps you safe by allowing friends and family to track your trips when you're alone.</p>
+			<img src="https://images.macrumors.com/t/9oWQqcEZSnpe7lxoLVneNJOe-vA=/800x0/article-new/2023/07/iOS-17-Messages-Feature.jpg?lossy" alt=""/>
+			<h2>Privacy and Security</h2>
+			<p>There are multiple privacy and security improvements in ‌iOS 17‌, including restricted access to the private browsing tab, the removal of tracking URLs, secure ways to share passwords, and more.</p>
+			<img src="https://images.macrumors.com/t/Mtr6qZp4G5cmsitDuMVCUXki2ho=/800x0/article-new/2023/06/iOS-17-Privacy-and-Security-Improvements-Feature.jpg?lossy" alt=""/>
+
+		</>
 	);
 };
